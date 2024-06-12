4d2cfc51cddf3ac3da9ffe99b8f70ba92ce4d639
Lucas Funes <lucas.funes@clear-tech.com>
2024-06-12 11:29:17 -0300

    Removing comments

--- api/core/database.py
+++ api/core/database.py
@@ -1,7 +1,7 @@
 import sqlite3
 from contextlib import closing
 
-DATABASE = '/app/data/data.db'  # Cambia el nombre del archivo si es necesario
+DATABASE = '/app/data/data.db'
 
 def query_db(query, args=(), one=False):
     with closing(sqlite3.connect(DATABASE)) as conn:
